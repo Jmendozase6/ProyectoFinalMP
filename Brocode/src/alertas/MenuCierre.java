dfb15917f6e1515157430e3f9bf1d2958b030dc0
Jmendozase6 <jmendozase6@ucvvirtual.edu.pe>
2021-10-03 21:14:24 -0500

    Login Terminado

---
+++ Brocode/src/alertas/MenuCierre.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alertas;
+
+import java.awt.Frame;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author JHAIR
+ */
+public class MenuCierre {
+
+    public void cerrarPrograma() {
+        if (JOptionPane.showConfirmDialog(null, "¿Desea salir?", "Comprobación", 0) == 0) {
+            System.exit(0);
+        }
+    }
+
+    public void minimizarPrograma(Frame frm) {
+        frm.setExtendedState(Frame.ICONIFIED);
+    }
+}
